8cf598b07c411f7231042b4922bd2a4cd9e7acb2
Akmal <septianaakmal222@gmail.com>
2025-02-04 07:20:36 +0700

    serkom

---
+++ penggajian/application/logs/log-2025-02-04.php
@@ -0,0 +1,94 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2025-02-04 00:17:31 --> Config Class Initialized
+INFO - 2025-02-04 00:17:31 --> Hooks Class Initialized
+DEBUG - 2025-02-04 00:17:31 --> UTF-8 Support Enabled
+INFO - 2025-02-04 00:17:31 --> Utf8 Class Initialized
+INFO - 2025-02-04 00:17:31 --> URI Class Initialized
+INFO - 2025-02-04 00:17:31 --> Router Class Initialized
+INFO - 2025-02-04 00:17:31 --> Output Class Initialized
+INFO - 2025-02-04 00:17:31 --> Security Class Initialized
+DEBUG - 2025-02-04 00:17:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-04 00:17:31 --> Input Class Initialized
+INFO - 2025-02-04 00:17:31 --> Language Class Initialized
+INFO - 2025-02-04 00:17:31 --> Loader Class Initialized
+INFO - 2025-02-04 00:17:31 --> Helper loaded: url_helper
+INFO - 2025-02-04 00:17:31 --> Helper loaded: form_helper
+INFO - 2025-02-04 00:17:31 --> Helper loaded: penggajian_helper
+INFO - 2025-02-04 00:17:31 --> Database Driver Class Initialized
+DEBUG - 2025-02-04 00:17:31 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-04 00:17:31 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-04 00:17:31 --> Form Validation Class Initialized
+INFO - 2025-02-04 00:17:31 --> Controller Class Initialized
+INFO - 2025-02-04 00:17:31 --> File loaded: C:\xampp\htdocs\SERKOM AKMAL 2025\penggajian\application\views\page404.php
+INFO - 2025-02-04 00:17:31 --> Final output sent to browser
+DEBUG - 2025-02-04 00:17:31 --> Total execution time: 0.0553
+INFO - 2025-02-04 00:17:32 --> Config Class Initialized
+INFO - 2025-02-04 00:17:32 --> Hooks Class Initialized
+DEBUG - 2025-02-04 00:17:32 --> UTF-8 Support Enabled
+INFO - 2025-02-04 00:17:32 --> Utf8 Class Initialized
+INFO - 2025-02-04 00:17:32 --> URI Class Initialized
+INFO - 2025-02-04 00:17:32 --> Router Class Initialized
+INFO - 2025-02-04 00:17:32 --> Output Class Initialized
+INFO - 2025-02-04 00:17:32 --> Security Class Initialized
+DEBUG - 2025-02-04 00:17:32 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-04 00:17:32 --> Input Class Initialized
+INFO - 2025-02-04 00:17:32 --> Language Class Initialized
+INFO - 2025-02-04 00:17:32 --> Loader Class Initialized
+INFO - 2025-02-04 00:17:32 --> Helper loaded: url_helper
+INFO - 2025-02-04 00:17:32 --> Helper loaded: form_helper
+INFO - 2025-02-04 00:17:32 --> Helper loaded: penggajian_helper
+INFO - 2025-02-04 00:17:32 --> Database Driver Class Initialized
+DEBUG - 2025-02-04 00:17:32 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-04 00:17:32 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-04 00:17:32 --> Form Validation Class Initialized
+INFO - 2025-02-04 00:17:32 --> Controller Class Initialized
+INFO - 2025-02-04 00:17:32 --> File loaded: C:\xampp\htdocs\SERKOM AKMAL 2025\penggajian\application\views\page404.php
+INFO - 2025-02-04 00:17:32 --> Final output sent to browser
+DEBUG - 2025-02-04 00:17:32 --> Total execution time: 0.0579
+INFO - 2025-02-04 00:17:32 --> Config Class Initialized
+INFO - 2025-02-04 00:17:32 --> Hooks Class Initialized
+DEBUG - 2025-02-04 00:17:32 --> UTF-8 Support Enabled
+INFO - 2025-02-04 00:17:32 --> Utf8 Class Initialized
+INFO - 2025-02-04 00:17:32 --> URI Class Initialized
+INFO - 2025-02-04 00:17:32 --> Router Class Initialized
+INFO - 2025-02-04 00:17:32 --> Output Class Initialized
+INFO - 2025-02-04 00:17:32 --> Security Class Initialized
+DEBUG - 2025-02-04 00:17:32 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-04 00:17:32 --> Input Class Initialized
+INFO - 2025-02-04 00:17:32 --> Language Class Initialized
+INFO - 2025-02-04 00:17:32 --> Loader Class Initialized
+INFO - 2025-02-04 00:17:32 --> Helper loaded: url_helper
+INFO - 2025-02-04 00:17:32 --> Helper loaded: form_helper
+INFO - 2025-02-04 00:17:32 --> Helper loaded: penggajian_helper
+INFO - 2025-02-04 00:17:32 --> Database Driver Class Initialized
+DEBUG - 2025-02-04 00:17:32 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-04 00:17:32 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-04 00:17:32 --> Form Validation Class Initialized
+INFO - 2025-02-04 00:17:32 --> Controller Class Initialized
+INFO - 2025-02-04 00:17:32 --> File loaded: C:\xampp\htdocs\SERKOM AKMAL 2025\penggajian\application\views\page404.php
+INFO - 2025-02-04 00:17:32 --> Final output sent to browser
+DEBUG - 2025-02-04 00:17:32 --> Total execution time: 0.0548
+INFO - 2025-02-04 00:17:32 --> Config Class Initialized
+INFO - 2025-02-04 00:17:32 --> Hooks Class Initialized
+DEBUG - 2025-02-04 00:17:32 --> UTF-8 Support Enabled
+INFO - 2025-02-04 00:17:32 --> Utf8 Class Initialized
+INFO - 2025-02-04 00:17:32 --> URI Class Initialized
+INFO - 2025-02-04 00:17:32 --> Router Class Initialized
+INFO - 2025-02-04 00:17:32 --> Output Class Initialized
+INFO - 2025-02-04 00:17:32 --> Security Class Initialized
+DEBUG - 2025-02-04 00:17:32 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-04 00:17:32 --> Input Class Initialized
+INFO - 2025-02-04 00:17:32 --> Language Class Initialized
+INFO - 2025-02-04 00:17:32 --> Loader Class Initialized
+INFO - 2025-02-04 00:17:32 --> Helper loaded: url_helper
+INFO - 2025-02-04 00:17:32 --> Helper loaded: form_helper
+INFO - 2025-02-04 00:17:32 --> Helper loaded: penggajian_helper
+INFO - 2025-02-04 00:17:32 --> Database Driver Class Initialized
+DEBUG - 2025-02-04 00:17:32 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-04 00:17:32 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-04 00:17:32 --> Form Validation Class Initialized
+INFO - 2025-02-04 00:17:32 --> Controller Class Initialized
+INFO - 2025-02-04 00:17:32 --> File loaded: C:\xampp\htdocs\SERKOM AKMAL 2025\penggajian\application\views\page404.php
+INFO - 2025-02-04 00:17:32 --> Final output sent to browser
+DEBUG - 2025-02-04 00:17:32 --> Total execution time: 0.0635
